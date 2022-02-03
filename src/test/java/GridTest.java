71cc72af073c3a5ffd40acdbc6aa118da873f685
Andrey2694 <fordcoppola94@gmail.com>
2022-02-03 21:15:31 +0200

    2e232

--- src/test/java/GridTest.java
+++ src/test/java/GridTest.java
@@ -2,6 +2,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -12,28 +13,23 @@ import java.net.URL;
 
 public class GridTest {
     public WebDriver driver;
-    String URL = "https://www.google.com";
+    String URL = "https://demoqa.com/";
     String Node = "http://0.0.0.0:4444/";
 
     @Test
     public void main() throws MalformedURLException {
         DesiredCapabilities caps = new DesiredCapabilities();
-        ChromeOptions chromeOptions = new ChromeOptions();
-        chromeOptions.addArguments("--start-maximized");
-        chromeOptions.addArguments("--no-sandbox");
-        chromeOptions.addArguments("--disable-infobars");
-        chromeOptions.addArguments("--disable-popup-blocking");
-        chromeOptions.addArguments("--disable-notifications");
-        chromeOptions.addArguments("--disable-dev-shm-usage");
-        chromeOptions.addArguments("--disable-setuid-sandbox");
-        chromeOptions.addArguments("--lang=en-en");
-        caps.setCapability(CapabilityType.BROWSER_NAME,"chrome");
-        caps.setCapability(ChromeOptions.CAPABILITY, chromeOptions);
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--start-maximized");
+        firefoxOptions.addArguments("--no-sandbox");
+        firefoxOptions.addArguments("--disable-infobars");
+        firefoxOptions.addArguments("--disable-popup-blocking");
+        firefoxOptions.addArguments("--disable-notifications");
+        firefoxOptions.addArguments("--disable-dev-shm-usage");
+        firefoxOptions.addArguments("--disable-setuid-sandbox");
+        caps.setCapability(ChromeOptions.CAPABILITY, firefoxOptions);
 
         driver = new RemoteWebDriver(new URL(Node), caps);
-//        WebDriverManager.chromedriver().setup();
-//        driver = new ChromeDriver();
-        // run against chrome
         driver.get(URL);
         System.out.println(driver.getTitle());
 
